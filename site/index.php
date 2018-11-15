6bac462e8a5cb0181f44f4cd5287d4ac0ecd712b
Toni Sredanovic <sirtonac@gmail.com>
2018-11-15 18:47:35 +0100

    Update index.php

--- site/index.php
+++ site/index.php
@@ -3,9 +3,8 @@
     <title>PHP Test</title>
   </head>
   <body>
-    <p>Before:</p>
     <?php
-      echo shell_exec('/usr/bin/python3 encryptappm.py "lenna.ppm" "out.ppm" "Finalno cini se da sve lepo radi i to je to" 2>&1');
+      echo shell_exec('/usr/bin/python3 encryptappm.py "lenna.ppm" "out.ppm" "Ja sam danas dosta dobar" 2>&1');
       echo shell_exec('/usr/bin/python3 decryptappm.py "out.ppm"');
     ?>
   </body>
