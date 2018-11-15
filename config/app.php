5f81c9d37b9fc6ab7bc5eb2181fe0de2e5b4d7d5
Toni Sredanovic <sirtonac@gmail.com>
2018-11-15 15:31:32 +0100

    rekonstrukcija

--- config/app.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return (object) array(
-    "title" => 'Crypt A Pic',
-    "timezone" => "Europe/Zagreb",
-    "key" => "/szCTtJOJjNNstt0r4Xe7RG5KAU5co1Uln2L/nRj9/I=",
-
-);
--- config/errors.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return (object) array(
-    "page_404" => "Slow down there buckaroo",
-    
-);
