a2e12f545a9b3c18f915137dee2502e9e19ad46a
Toni Sredanovic <sirtonac@gmail.com>
2018-12-05 18:15:25 +0100

    Update encryptappm.py

--- site/encryptappm.py
+++ site/encryptappm.py
@@ -14,8 +14,11 @@ def main():
         byte_list_with_message = cryptappm.hide_message(byte_list, data_index, cryptappm.secret_id, message_size_in_bits, message_in_bits)
         cryptappm.bytes_to_file(output_file_name, byte_list_with_message)
 
+        print("True")
         return 0
-    except:
+    except Exception as e:
+
+        print("False")
         return 1
 
 if __name__ == '__main__':
